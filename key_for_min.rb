1e453878435525537b95ad4db1c1e2aecd496941
Christina Collura <cmcollura@gmail.com>
2018-01-07 19:55:27 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,8 @@ def key_for_min_value(name_hash)
   low_k = nil
     name_hash.each do |key, value|
       if low_v == nil || value < low_v
-        
+        low_v = value
+        low_k = key
       
     end  
 end
